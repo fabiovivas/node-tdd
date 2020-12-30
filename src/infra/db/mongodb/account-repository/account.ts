3af1139eecea43b4bca83618208ac6ea15590691
fabiovivas <fvivas1982@gmail.com>
2020-12-30 17:05:05 -0300

    create tests to LoadByEmailRepository

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,12 +1,19 @@
 import { AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
+import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-email-by-repository'
 import { AccountModel } from '../../../../domain/model/account-model'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
     async add(accountData: AddAccountModel): Promise<AccountModel> {
         const accountCollection = await MongoHelper.getCollection('accounts')
         const result = await accountCollection.insertOne(accountData)
         return MongoHelper.map(result.ops[0])
+    }
+
+    async loadByEmail(email: string): Promise<AccountModel> {
+        const accountCollection = await MongoHelper.getCollection('accounts')
+        const result = await accountCollection.findOne({ email })
+        return result && MongoHelper.map(result)
     }
 }
