11d90a164acaf55bf6bba6e23aca684b01b3871a
fabiovivas <fvivas1982@gmail.com>
2020-12-31 07:43:49 -0300

    ensure Login route return 401 if provided invalid credentials

--- src/main/routes/login.routes.test.ts
+++ src/main/routes/login.routes.test.ts
@@ -51,5 +51,15 @@ describe('Login Routes', () => {
                 })
                 .expect(200)
         })
+
+        test('Should return 401 if provided invalid credentials', async () => {
+            await request(app)
+                .post('/api/login')
+                .send({
+                    email: 'fviva1982@gmail.com',
+                    password: '123'
+                })
+                .expect(401)
+        })
     })
 })
