8b7d25692ccd6bf2364e1f16cb6bda4354bc93be
fabiovivas <fvivas1982@gmail.com>
2021-01-07 14:21:46 -0300

    separete factories in folders

---
+++ src/main/factories/usecases/authentication/db-authentication-factory.ts
@@ -0,0 +1,14 @@
+import { DbAuthentication } from '../../../../data/usecases/authentication/db-authentication'
+import { Authentication } from '../../../../domain/usecases/authentication'
+import { BcriptyAdapter } from '../../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
+import { JwtAdapter } from '../../../../infra/criptography/jwt-adpter/jwt-adapter'
+import { AccountMongoRepository } from '../../../../infra/db/mongodb/account/account-mongo-repository'
+import env from '../../../config/env'
+
+export const makeDbAuthentication = (): Authentication => {
+    const salt = 12
+    const bcrypterAdapter = new BcriptyAdapter(salt)
+    const jwtAdapter = new JwtAdapter(env.jwtSecret)
+    const accountMongoRepository = new AccountMongoRepository()
+    return new DbAuthentication(accountMongoRepository, bcrypterAdapter, jwtAdapter, accountMongoRepository)
+}
