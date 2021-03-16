5dcac084038aadd434d5df98a555b1e00b21f907
Fabio Vivas <fvivas1982@gmail.com>
2021-03-16 13:06:22 -0300

    add usecases for surveyResult

---
+++ src/domain/usecases/save-survey-result.ts
@@ -0,0 +1,6 @@
+import { SurveyResultModel } from '../model/survey-result-model'
+
+export type SaveSurveyResultModel = Omit<SurveyResultModel, 'id'>
+export interface SaveSurveyResult {
+    save: (data: SaveSurveyResultModel) => Promise<SurveyResultModel>
+}
