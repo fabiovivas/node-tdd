0080982bc43831d71770df1ca087f7f6e4c6818d
Fabio Vivas <fvivas1982@gmail.com>
2021-03-17 09:37:01 -0300

    ensure SurveyResultMongoRepository update a survey result if its not new

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
@@ -1,9 +1,8 @@
-import { Collection } from 'mongodb'
+import { Collection, ObjectId } from 'mongodb'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { SurveyResultMongoRepository } from './survey-result-mongo-repository'
 import MockDate from 'mockdate'
 import { SurveyModel } from '@/domain/model/survey-model'
-import { AccountModel } from '@/domain/model/account-model'
 
 let surveyCollection: Collection
 let surveyResultCollection: Collection
@@ -15,19 +14,22 @@ const makeSurvey = async (): Promise<SurveyModel> => {
         answers: [{
             image: 'any_image',
             answer: 'any_answer'
+        }, {
+            image: 'other_image',
+            answer: 'other_answer'
         }],
         date: new Date()
     })
-    return res.ops[0]
+    return MongoHelper.map(res.ops[0])
 }
 
-const makeAccount = async (): Promise<AccountModel> => {
+const makeAccountId = async (): Promise<string> => {
     const res = await accountCollection.insertOne({
         name: 'any_email',
         email: 'any_email',
         password: 'any_password'
     })
-    return res.ops[0]
+    return res.ops[0]._id
 }
 
 describe('Survey Mongo Repository', () => {
@@ -60,19 +62,36 @@ describe('Survey Mongo Repository', () => {
         test('Should add a survey result if its new', async () => {
             const sut = makeSut()
             const survey = await makeSurvey()
-            const account = await makeAccount()
+            const accountId = await makeAccountId()
             const surveyResult = await sut.save({
                 surveyId: survey.id,
-                accountId: account.id,
+                accountId,
                 answer: survey.answers[0].answer,
                 date: new Date()
             })
-            const result = await surveyResultCollection.findOne({
-                surveyId: survey.id,
-                accountId: account.id
-            })
-            expect(result).toBeTruthy()
+            expect(surveyResult).toBeTruthy()
             expect(surveyResult.answer).toBe(survey.answers[0].answer)
+        })
+
+        test('Should update a survey result if its not new', async () => {
+            const survey = await makeSurvey()
+            const accountId = await makeAccountId()
+            const res = await surveyResultCollection.insertOne({
+                surveyId: new ObjectId(survey.id),
+                accountId: new ObjectId(accountId),
+                answer: survey.answers[0].answer,
+                date: new Date()
+            })
+            const sut = makeSut()
+            const surveyResult = await sut.save({
+                surveyId: survey.id,
+                accountId: accountId,
+                answer: survey.answers[1].answer,
+                date: new Date()
+            })
+            expect(surveyResult).toBeTruthy()
+            expect(surveyResult.id).toEqual(res.ops[0]._id)
+            expect(surveyResult.answer).toBe(survey.answers[1].answer)
         })
     })
 })
--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -1,14 +1,15 @@
 import { SaveSurveyResultRepository } from '@/data/protocols/db/survey/save-survey-result-repository'
 import { SurveyResultModel } from '@/domain/model/survey-result-model'
 import { SaveSurveyResultModel } from '@/domain/usecases/save-survey-result'
+import { ObjectId } from 'mongodb'
 import { MongoHelper } from '../helpers/mongo-helper'
 
 export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
     async save(data: SaveSurveyResultModel): Promise<SurveyResultModel> {
         const surveyResultCollection = await MongoHelper.getCollection('surveyResults')
         const res = await surveyResultCollection.findOneAndUpdate({
-            surveyId: data.surveyId,
-            accountId: data.accountId
+            surveyId: new ObjectId(data.surveyId),
+            accountId: new ObjectId(data.accountId)
         }, {
             $set: {
                 answer: data.answer,
