628c171c3111f96f24001761678d48d8824055e2
Fabio Vivas <fvivas1982@gmail.com>
2021-03-20 18:23:36 -0300

    update login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,5 +1,7 @@
 import { loginPath } from './paths/login-path'
+import { badRequest, unauthorized, serverError } from './components/index'
 import { accountSchema } from './schemas/account.schema'
+import { errorSchema } from './schemas/error.schema'
 import { loginParamsSchema } from './schemas/login-params.schema'
 
 export default {
@@ -8,6 +10,10 @@ export default {
         title: 'Clean Node Api',
         description: 'API do curso do mango para realizar enquetes entre programadores',
         version: '1.0.0'
+    },
+    license: {
+        name: 'GPL-3.0-or-later',
+        url: 'https://spdx.org/licenses/GPL-3.0-or-later.html'
     },
     servers: [{
         url: '/api'
@@ -20,6 +26,10 @@ export default {
     },
     schemas: {
         account: accountSchema,
-        loginParams: loginParamsSchema
+        loginParams: loginParamsSchema,
+        error: errorSchema
+    },
+    components: {
+        badRequest, unauthorized, serverError
     }
 }
